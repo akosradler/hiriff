6ada3ae3d3b5db70a36d4614eea8352c35eb02cb
Akos Radler <akos.radler@gmail.com>
2019-11-16 18:54:58 +0100

    Changing versions

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:qr_scanner_generator/scan.dart';
-import 'package:qr_scanner_generator/generate.dart';
 import 'package:flutter/rendering.dart';
+import './ScanScreen.dart';
 
 class HomeScreen extends StatelessWidget {
 
